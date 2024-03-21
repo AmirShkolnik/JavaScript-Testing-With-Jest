7206299a59c534f6837e8412430dde14e64b3fb0
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-03-21 20:40:34 +0000

    game.js - change function places

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
@@ -16,18 +17,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
-}
-
-function showScore() {
-    document.getElementById("score").innerText = game.score;
-}
-
-function lightsOn(circ) {
-    document.getElementById(circ).classList.add("light");
-    setTimeout(function () {
-        document.getElementById(circ).classList.remove("light");
-    }, 400);
+    showTurns();
 }
 
 function showTurns() {
@@ -41,4 +31,15 @@ function showTurns() {
     }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
